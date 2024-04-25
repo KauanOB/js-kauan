40e2326fd6ea73b3f6b80be79cb267d43610a8fe
KauanOB <168047128+KauanOB@users.noreply.github.com>
2024-04-24 22:39:39 -0300

    adcionando arquivos js e css

---
+++ skecth.js
@@ -0,0 +1,10 @@
+// Função para exibir uma mensagem de saudação
+function exibirSaudacao() {
+    var saudacao = "Olá, mundo!";
+    alert(saudacao);
+}
+
+// Chamando a função de saudação quando a página é carregada
+window.onload = function() {
+    exibirSaudacao();
+};
